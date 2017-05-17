dad332d23f89825f0d09256418fb1804d21d6aba
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-05-17 14:12:24 +0200

    ex54 first commit

---
+++ ActorList.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Actor {
+	String name;
+	String role;
+	String birthdate;
+}
+
+public class ActorList {
+	public static void main(String[] args) throws Exception {
+		String url = "https://learnjavathehardway.org/txt/s01e01-cast.txt";
+		// Scanner inFile = new Scanner(new javaio.File
+		//("s01e01-cast.txt"))
+		Scanner inFile = new Scanner((new java.net.URL(url)).openStream());
+		int counter = 0;
+		while (inFile.hasNext()) {
+			Actor a = getActor(inFile);
+			counter++;
+			System.out.print(a.name + " was born on " + a.birthdate);
+			System.out.println(" and played " + a.role);
+		}
+		//SD: Had to be outside the loop.
+		System.out.println(counter + " records read.");
+		inFile.close();
+	} 
+
+	public static Actor getActor(Scanner input) {
+		Actor a = new Actor();
+		a.name = input.nextLine();
+		a.role = input.nextLine();
+		a.birthdate = input.nextLine();
+
+		return a;
+	}
+}
